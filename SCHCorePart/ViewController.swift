a1fb79575e4e1930cf6c7eb2bac07a1a81b047f6
David <schooldave@sina.com>
2020-05-12 19:45:04 +0800

    Initial Commit

---
+++ SCHCorePart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCHCorePart
+//
+//  Created by schooldave on 5/12/20.
+//  Copyright © 2020 schooldave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
