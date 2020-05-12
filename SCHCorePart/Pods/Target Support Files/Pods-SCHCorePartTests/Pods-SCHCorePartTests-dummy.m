15fc467b38e3976db0c50f7377a7f15866093c32
David <schooldave@sina.com>
2020-05-12 22:17:33 +0800

    update

---
+++ SCHCorePart/Pods/Target Support Files/Pods-SCHCorePartTests/Pods-SCHCorePartTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SCHCorePartTests : NSObject
+@end
+@implementation PodsDummy_Pods_SCHCorePartTests
+@end
---
+++ SCHCorePart/SCHCorePart/SCHPerson.swift
@@ -0,0 +1,13 @@
+//
+//  SCHPerson.swift
+//  SCHCorePart
+//
+//  Created by schooldave on 5/12/20.
+//  Copyright © 2020 schooldave. All rights reserved.
+//
+
+import UIKit
+
+class SCHPerson: NSObject {
+
+}
