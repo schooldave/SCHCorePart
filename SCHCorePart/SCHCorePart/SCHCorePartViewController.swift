ba1eb81b5b424ece9e2a9957c88ca07be33fb137
David <schooldave@sina.com>
2020-05-12 21:21:50 +0800

    SCHCorePartViewControllerSet

---
+++ SCHCorePart/SCHCorePart/SCHCorePartViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SCHCorePartViewController.swift
+//  SCHCorePart
+//
+//  Created by schooldave on 5/12/20.
+//  Copyright © 2020 schooldave. All rights reserved.
+//
+
+import UIKit
+
+class SCHCorePartViewController: UIViewController {
+    
+    public var labelText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.orange
+        title = "SCHCorePartViewController"
+    
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        label.backgroundColor = UIColor.green
+        label.textColor = UIColor.black
+        label.text = labelText
+        view.addSubview(label)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
